134366b9c28b58d696e9c9b6b5c543b886b29064
Saharat-GH <138373768+Saharat-GH@users.noreply.github.com>
2024-01-19 10:51:22 +0700

    fix editHr.jsx

--- src/routes/editHr.jsx
+++ src/routes/editHr.jsx
@@ -5,80 +5,60 @@ import {Link} from "react-router-dom";
 
 function Edit() {
   const { id } = useParams();
-  const [values, setValues] = useState({
+  const [hrs, setHrs] = useState({
     id: id,
     firstName: '',
     lastName: '',
     email: '',
     password: '',
-    project:{
-      id:''
-    },
   })
+
   useEffect(() => {
     axios.get('http://localhost:8080/api/hr/' + id)
       .then(res => {
-        setValues({
-          ...values,
-          id: res.data.id,
-          firstName: res.data.firstName,
-          lastName: res.data.lastName,
-          email: res.data.email,
-          password: res.data.password,
-          project: {
-            id: res.data.project.id
-          }
-        })
-        // console.log(values.project);
+        setHrs(res.data);
+        console.log("fetch hr successfully!");
       })
-      .catch(err => console.log(err))
-  }, [])
-
+      .catch(err => console.log("fecting hr error: " + err))
+    }, [])
+    
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.put('http://localhost:8080/api/hr/' + id, values)
-      .then(res => {
-        navigate('/')
+    axios.put('http://localhost:8080/api/hr/' + id, hrs)
+    .then(res => {
+        navigate(-1)
       })
-      .catch(err => console.log(err.response.data))
-  }
+      .catch(err => console.log("put hr error: " + err.response.data))
+    }
 
   return (
     <div className='d-flex w-100 vh-100 justify-content-center align-items-center'>
       <div className='w-50 border bg-dark text-white p-5'>
         <form onSubmit={handleSubmit}>
           <div>
-            <h1>EDIT HR</h1>
-            <label htmlFor='id'>ID Number:</label>
-            <input disabled type='text' name='id' className='form-control' placeholder='Enter ID Number'
-              value={values.id} onChange={e => setValues({ ...values, id: e.target.value })} />
+            <h1>{`EDIT HR ID: ${hrs.id}`}</h1>
           </div>
           <div>
-            <label htmlFor='fname'>First Name:</label>
+            <label htmlFor='firstName'>First Name:</label>
             <input type='text' name='firstName' className='form-control' placeholder='Enter First Name'
-              value={values.firstName} onChange={e => setValues({ ...values, firstName: e.target.value })} />
+              value={hrs.firstName} onChange={e => setHrs({ ...hrs, firstName: e.target.value })} />
           </div>
           <div>
-            <label htmlFor='lname'>Last Name:</label>
+            <label htmlFor='lastName'>Last Name:</label>
             <input type='text' name='lastName' className='form-control' placeholder='Enter Last Name'
-              value={values.lastName} onChange={e => setValues({ ...values, lastName: e.target.value })} />
+              value={hrs.lastName} onChange={e => setHrs({ ...hrs, lastName: e.target.value })} />
           </div>
           <div>
             <label htmlFor='email'>Email:</label>
             <input type='email' name='email' className='form-control' placeholder='Enter Email'
-              value={values.email} onChange={e => setValues({ ...values, email: e.target.value })} />
+              value={hrs.email} onChange={e => setHrs({ ...hrs, email: e.target.value })} />
           </div>
           <div>
             <label htmlFor='password'>Password:</label>
-            <input type='password' name='password' className='form-control' placeholder='Enter Password'
-              value={values.password} onChange={e => setValues({ ...values, password: e.target.value })} />
-          </div>
-          <div>
-            <label htmlFor='pid'>Project ID:</label>
-            <input type='number' name='pid' className='form-control' placeholder='Enter Project ID'
-              value={values.project.id} onChange={e => setValues({ ...values, project: { id: e.target.value} })} />
+            <input type='text' name='password' className='form-control' placeholder='Enter Password'
+              value={hrs.password} onChange={e => setHrs({ ...hrs, password: e.target.value })} />
           </div>
           <br />
           <button className='btn btn-warning'>Update</button>
