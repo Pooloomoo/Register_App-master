ae7959ca23bfb7e00f2f5032652b225a6987a0fe
Saharat-GH <138373768+Saharat-GH@users.noreply.github.com>
2024-01-19 11:03:13 +0700

    fix createHr.jsx

--- src/routes/createHr.jsx
+++ src/routes/createHr.jsx
@@ -4,86 +4,63 @@ import axios from 'axios';
 import {Link} from "react-router-dom";
 
 function CreateHr() {
-  const { id } = useParams();
-  const [values, setValues] = useState({
-    id: 0,
+  const id = 0;
+  const [hrs, setHrs] = useState({
+    id: id,
     firstName: '',
     lastName: '',
     email: '',
     password: '',
-    project: {
-      id: ''
-    },
-  })
-  useEffect(() => {
-    axios.get('http://localhost:8080/api/hr/')
-      .then(res => {
-        // setValues({
-        //   ...values,
-        //   id: res.data.id,
-        //   firstName: res.data.firstName,
-        //   lastName: res.data.lastName,
-        //   email: res.data.email,
-        //   password: res.data.password,
-        //   project: {
-        //     id: res.data.project.id
-        //   }
-        // })
-        // console.log(values.project);
-      })
-      .catch(err => console.log(err))
-  }, [])
+  });
 
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('http://localhost:8080/api/hr/', values)
+    axios.post('http://localhost:8080/api/hr/', hrs)
       .then(res => {
-        navigate('/admin/hr')
+        console.log("post new HR success!");
+        navigate('/admin/hr');
         // console.log("create hr!")
       })
-      .catch(err => console.log(err.response.data))
+      .catch(err => console.log("post HR error: " + err.response.data))
   }
 
   return (
     <div className='d-flex w-100 vh-100 justify-content-center align-items-center'>
       <div className='w-50 border bg-dark text-white p-5'>
         <form onSubmit={handleSubmit}>
-          {/* <div>
+          <div>
             <h1>CREATE HR</h1>
-            <label htmlFor='id'>ID Number:</label>
-            <input type='text' name='id' className='form-control' placeholder='Enter ID Number'
-              onChange={e => setValues({ ...values, id: e.target.value })} />
-          </div> */}
+          </div>
           <div>
             <label htmlFor='fname'>First Name:</label>
             <input type='text' name='firstName' className='form-control' placeholder='Enter First Name'
-              onChange={e => setValues({ ...values, firstName: e.target.value })} />
+              onChange={e => setHrs({ ...hrs, firstName: e.target.value })} />
           </div>
           <div>
             <label htmlFor='lname'>Last Name:</label>
             <input type='text' name='lastName' className='form-control' placeholder='Enter Last Name'
-              onChange={e => setValues({ ...values, lastName: e.target.value })} />
+              onChange={e => setHrs({ ...hrs, lastName: e.target.value })} />
           </div>
           <div>
             <label htmlFor='email'>Email:</label>
             <input type='email' name='email' className='form-control' placeholder='Enter Email'
-              onChange={e => setValues({ ...values, email: e.target.value })} />
+              onChange={e => setHrs({ ...hrs, email: e.target.value })} />
           </div>
           <div>
             <label htmlFor='password'>Password:</label>
             <input type='password' name='password' className='form-control' placeholder='Enter Password'
-              onChange={e => setValues({ ...values, password: e.target.value })} />
+              onChange={e => setHrs({ ...hrs, password: e.target.value })} />
           </div>
           {/* <div>
             <label htmlFor='pid'>Project ID:</label>
             <input type='number' name='pid' className='form-control' placeholder='Enter Project ID'
-              onChange={e => setValues({ ...values, project: { id: e.target.value } })} />
+              onChange={e => setHrs({ ...hrs, project: { id: e.target.value } })} />
           </div> */}
           <br />
           <button className='btn btn-warning'>CREATE</button>
-          <Link className="btn-group text-decoration-none"  to={`/`}>
+          <Link className="btn-group text-decoration-none"  to={`/admin/hr`}>
                 <button id="button1" className="btn btn-warning">Cancel</button>
           </Link>
         </form>
